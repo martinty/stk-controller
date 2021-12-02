75839243b768c4badce7af7ce66f4ccdb94e30e6
Martin Tysseland <martinty@sqhead.com>
2021-12-02 14:07:34 +0100

    Add dry-run feature

--- server.py
+++ server.py
@@ -46,6 +46,28 @@ CONTROLLER = {
 }
 
 
+class DummyKeyboardController(object):
+    @staticmethod
+    def get_key_as_string(key):
+        if key == Key.up:
+            return "up"
+        if key == Key.down:
+            return "down"
+        if key == Key.left:
+            return "left"
+        if key == Key.right:
+            return "right"
+        if key == Key.space:
+            return "space"
+        return key
+
+    def press(self, key):
+        print("pressed", self.get_key_as_string(key))
+
+    def release(self, key):
+        print("released", self.get_key_as_string(key))
+
+
 def control_player(keyboard, player: int, acc: int, dir: int, act: int) -> None:
     # Acceleration
     if acc < 0:
@@ -82,8 +104,11 @@ def release_all_keys(keyboard) -> None:
             keyboard.release(k)
 
 
-def main(protocol) -> None:
-    keyboard = Controller()
+def main(protocol, dry_run) -> None:
+    if dry_run:
+        keyboard = DummyKeyboardController()
+    else:
+        keyboard = Controller()
 
     def process_command(data: str) -> None:
         player_input = data.split(",")
@@ -107,5 +132,6 @@ def main(protocol) -> None:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--protocol", choices=["http", "tcp", "udp"], default="http")
+    parser.add_argument("--dry-run", action="store_true")
     args = parser.parse_args()
-    main(args.protocol)
+    main(args.protocol, args.dry_run)
